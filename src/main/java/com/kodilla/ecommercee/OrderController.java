139a543bc7bf09eceb6b3a6a5308f499995ba920
SebaaK <sebaa.kot@gmail.com>
2022-01-03 14:00:54 +0100

    Created pre-test enpoints OrderController. This controller dont use repository

--- src/main/java/com/kodilla/ecommercee/OrderController.java
+++ src/main/java/com/kodilla/ecommercee/OrderController.java
@@ -1,4 +1,60 @@
 package com.kodilla.ecommercee;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("/orders")
 public class OrderController {
+
+    @GetMapping
+    public ResponseEntity<List<GenericEntity>> getAllOrders() {
+        List<GenericEntity> temporaryList = Arrays.asList(
+                new GenericEntity("Order 1"),
+                new GenericEntity("Order 2"),
+                new GenericEntity("Order 3")
+        );
+
+        return new ResponseEntity(
+                temporaryList,
+                HttpStatus.OK
+        );
+    }
+
+    @PostMapping
+    public ResponseEntity<GenericEntity> addNewOrder(@RequestBody final GenericEntity genericEntity) {
+        return new ResponseEntity(
+                genericEntity + " is created",
+                HttpStatus.CREATED
+        );
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<GenericEntity> getOrder(@PathVariable("id") final Long id) {
+        return new ResponseEntity(
+                "Order by id: " + new GenericEntity("Searching order by id"),
+                HttpStatus.OK
+        );
+    }
+
+    @PutMapping
+    public ResponseEntity<Object> updateOrder(@RequestBody final GenericEntity genericEntity) {
+        return new ResponseEntity(
+                "Updated order: " + genericEntity,
+                HttpStatus.OK
+        );
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<GenericEntity> deleteOrder(@PathVariable("id") final Long id) {
+        return new ResponseEntity(
+                "Order by id " + id + " was deleted",
+                HttpStatus.OK
+        );
+    }
 }
