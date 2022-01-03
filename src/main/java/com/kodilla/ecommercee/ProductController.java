6441254f843cadb075c008bbb62086c97ec46dfd
baalperez <baal_perez@wp.pl>
2022-01-03 21:37:08 +0100

    ProductController - initial endpoint implementation

--- src/main/java/com/kodilla/ecommercee/ProductController.java
+++ src/main/java/com/kodilla/ecommercee/ProductController.java
@@ -1,4 +1,46 @@
 package com.kodilla.ecommercee;
 
+import com.kodilla.ecommercee.dto.ProductDto;
+import org.springframework.web.bind.annotation.*;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
 public class ProductController {
+
+    @GetMapping(value="/getProducts")
+    public List<ProductDto> getProducts() {
+        List<ProductDto> productDtoList = new ArrayList<>();
+        productDtoList.add(new ProductDto(1L, "cheese", "price for 1 kg",
+                new BigDecimal("11.99")));
+        productDtoList.add(new ProductDto(2L, "apple", "price per item",
+                new BigDecimal("1.99")));
+        productDtoList.add(new ProductDto(3L, "potatoes", "price for 1 kg",
+                new BigDecimal("4.99")));
+        return productDtoList;
+    }
+
+    @GetMapping(value="getProduct")
+    public ProductDto getProduct(@RequestParam final Long id) {
+        return new ProductDto(1L, "cheese", "price for 1 kg",
+                new BigDecimal("11.99"));
+    }
+
+    @PostMapping(value="createProduct")
+    public ProductDto addProduct(@RequestBody final ProductDto product) {
+        return product;
+    }
+
+    @PutMapping(value="updateProduct")
+    public ProductDto updateProduct(@RequestBody final ProductDto product) {
+        return product;
+    }
+
+    @DeleteMapping(value="deleteProduct")
+    public String deleteProduct(@RequestParam final Long id) {
+        return "The product with id " + id + " has been deleted";
+    }
+
 }
