302c61bd66db5f2f7f1d59a8ed39737749fd9b2e
Joanna Sokolowska <joanna_s@op.pl>
2022-01-04 20:00:52 +0100

    Added Order Entity, created Cart and User to set relation - correct

---
+++ src/main/java/com/kodilla/ecommercee/repository/OrderRepository.java
@@ -0,0 +1,12 @@
+package com.kodilla.ecommercee.repository;
+
+import com.kodilla.ecommercee.domain.Order;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface OrderRepository extends CrudRepository<Order, Long> {
+
+    @Override
+    List<Order> findAll();
+}
