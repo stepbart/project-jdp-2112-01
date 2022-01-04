e7e5945c05f7f7971e2e64fc1dd8e9ce08a25791
Bartosz Zemsta <zemstabartosz@gmail.com>
2022-01-04 13:55:20 +0100

    updated group controller + DTO

--- src/main/java/com/kodilla/ecommercee/group/controller/GroupController.java
+++ src/main/java/com/kodilla/ecommercee/group/controller/GroupController.java
@@ -1,41 +1,68 @@
 package com.kodilla.ecommercee.group.controller;
 
-import com.kodilla.ecommercee.GenericEntity;
-import com.kodilla.ecommercee.GenericEntityRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.kodilla.ecommercee.dto.GroupDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping(value = "/groups")
 public class GroupController {
 
-    @Autowired
-    private GenericEntityRepository genericEntityRepository;
+    private GroupDto groupDto;
 
     @GetMapping("/all")
-    public List<GenericEntity> getAllGroups() {
-        List<GenericEntity> all = genericEntityRepository.findAll();
-        return all;
+    public ResponseEntity<List<GroupDto>> getAllGroups() {
+        List<GroupDto> groups = new ArrayList<>();
+
+        for(int x = 0; x <= 10; x++) {
+            groups.add(x,
+                    new GroupDto(
+                            (long) x,
+                            "GroupName#" +x
+                    ));
+        }
+        if(groups.size() == 0) {
+            return new ResponseEntity(
+                    groups,
+                    HttpStatus.INTERNAL_SERVER_ERROR
+            );
+        } else
+            return new ResponseEntity(
+                groups,
+                HttpStatus.OK
+        );
+
     }
 
     @GetMapping("/{id}")
-    public GenericEntity getGroupById(@PathVariable("id") final Long id) {
-        GenericEntity generic = genericEntityRepository.findById(id).orElseThrow(null);
-        return generic;
+    public ResponseEntity<GroupDto> getGroupById(@PathVariable("id") final Long id) {
+
+        return new ResponseEntity(
+                new GroupDto(
+                        id,
+                        "Group#"+id
+                ), HttpStatus.OK
+        );
     }
 
     @PostMapping("/addGroup")
-    public GenericEntity addNewGroup(@RequestBody final GenericEntity genericEntity) {
-        GenericEntity save = genericEntityRepository.save(genericEntity);
-        return save;
+    public ResponseEntity<GroupDto> addNewGroup(@RequestBody final GroupDto groupDto) {
+        return new ResponseEntity(
+                groupDto,
+                HttpStatus.CREATED
+        );
     }
 
     @PutMapping("/updateGroup")
-    public GenericEntity updateGroup(@RequestBody GenericEntity genericEntity) {
-        GenericEntity updated = genericEntityRepository.saveAndFlush(genericEntity);
-        return updated;
+    public ResponseEntity<GroupDto> updateGroup(@RequestBody GroupDto groupDto) {
+        return new ResponseEntity(
+                groupDto,
+                HttpStatus.ACCEPTED
+        );
     }
 
 }
